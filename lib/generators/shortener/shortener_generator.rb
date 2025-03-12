d4a38d8f363d85dd1231fc20c48ea0d68d5631e7
Rishi Banerjee <rishieric91@gmail.com>
2025-03-12 16:04:30 +0530

    Fix Rails 8 migration generator for Shortener

--- lib/generators/shortener/shortener_generator.rb
+++ lib/generators/shortener/shortener_generator.rb
@@ -5,11 +5,11 @@ class ShortenerGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
 
   def self.source_root
-    @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+    File.expand_path('templates', __dir__)
   end
 
   def self.next_migration_number(dirname)
-    if ActiveRecord.respond_to?(:timestamped_migrations) ? ActiveRecord.timestamped_migrations : ActiveRecord::Base.timestamped_migrations
+    if ActiveRecord::Base.connection.migration_context.timestamped_migrations
       Time.new.utc.strftime("%Y%m%d%H%M%S")
     else
       "%.3d" % (current_migration_number(dirname) + 1)
@@ -19,5 +19,4 @@ class ShortenerGenerator < Rails::Generators::Base
   def create_migration_file
     migration_template 'migration.rb', 'db/migrate/create_shortened_urls_table.rb'
   end
-
 end
