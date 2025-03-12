fb0a6ad13cc5f4908fa43ce792b8a6aeda99ef09
Rishi Banerjee <rishieric91@gmail.com>
2025-03-12 16:06:35 +0530

    fix next migration number
    Signed-off-by: Rishi Banerjee <rishieric91@gmail.com>

--- lib/generators/shortener/shortener_generator.rb
+++ lib/generators/shortener/shortener_generator.rb
@@ -9,10 +9,10 @@ class ShortenerGenerator < Rails::Generators::Base
   end
 
   def self.next_migration_number(dirname)
-    if ActiveRecord::Base.connection.migration_context.timestamped_migrations
-      Time.new.utc.strftime("%Y%m%d%H%M%S")
+    if ActiveRecord::Base.timestamped_migrations
+      Time.now.utc.strftime("%Y%m%d%H%M%S")
     else
-      "%.3d" % (current_migration_number(dirname) + 1)
+      format("%.3d", current_migration_number(dirname) + 1)
     end
   end
 
